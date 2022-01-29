6feb9d7ae00570b1511d0abe646a5f757f31dd1a
ralmayer <llev4n@gmail.com>
2022-01-29 18:26:58 +0400

    revisiting theory

---
+++ cs/linked lists/ll.js
@@ -0,0 +1,51 @@
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+    this.getLast = this.getLast.bind(this);
+  }
+
+  //   getLast() {
+  //     let pointer = this.head;
+  //     if (this.head) {
+  //       while (pointer.next) {
+  //         pointer = pointer.next;
+  //       }
+  //     }
+  //     return pointer;
+  //   }
+
+  getLast(node = this.head) {
+    // let self = this;
+    if (!node) {
+      return null;
+    }
+    if (!node.next) {
+      return node;
+    }
+    return this.getLast(node.next);
+  }
+
+  addNew(node) {
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let last = this.getLast(this.head);
+    last.next = node;
+    return last.next;
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+let list = new LinkedList(new Node(1));
+list.addNew(new Node(2));
+list.addNew(new Node(3));
+list.addNew(new Node(4));
+list.addNew(new Node(5));
+console.log(list.getLast());
